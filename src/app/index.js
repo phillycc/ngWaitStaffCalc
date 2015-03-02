4dcb0a7a7b705b4114fee9f3de61f54ad1bccdc9
David Elmer <davelmer@hotmail.com>
2015-03-02 11:29:22 -0500

    Added initial scaffolding

---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('ngWaitStaffCalc', ['ngAnimate', 'ngRoute'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+;
---
+++ src/components/navbar/navbar.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('ngWaitStaffCalc')
+  .controller('NavbarCtrl', function ($scope) {
+    $scope.date = new Date();
+  });
